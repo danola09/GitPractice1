a5bf0fe913152e9364bbff9f61305b74c27b5d38
VMWarePratice <dano_la@yahoo.com>
2022-12-14 22:04:22 -0500

    added Test1.java file, updated print

--- src/Test1.java
+++ src/Test1.java
@@ -1,2 +1,8 @@
+import org.w3c.dom.ls.LSOutput;
+
 public class Test1 {
+
+    public static void main(String[] args) {
+        System.out.println("We are moving forward with learning more tools as a SDET");
+    }
 }
